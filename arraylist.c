a76a497fbd1f77c0cfa0624cce7a7d7d7dbe76fd
Replit user <>
2023-03-31 11:33:29 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -13,7 +13,7 @@ typedef struct ArrayList {
 ArrayList *createList(void) {
   ArrayList* lista = (ArrayList*) malloc(sizeof(ArrayList));
   lista -> capacity = 2;
-  lista -> size = 1;
+  lista -> size = 0;
   lista -> data = malloc(sizeof(ArrayList));
   
     return lista;
